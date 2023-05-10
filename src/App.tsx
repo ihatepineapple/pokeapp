3393122ea2073e1bda7a0832c886ff3ab15be34c
ireneasensio <irene.asensio@dentsu.com>
2023-05-10 18:43:23 +0200

    initial comit for tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useGetPokemonByNameQuery } from "./services/pokemon";
 
 function App() {
+  const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
+
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h3>{data.species?.name}</h3>
+          <img src={data.sprites?.front_shiny} alt={data.species?.name} />
+        </>
+      ) : null}
     </div>
   );
 }
---
+++ src/services/types.ts
@@ -0,0 +1,7 @@
+export type Pokemon = {
+  name: string;
+  species: () => void;
+  sprites: {
+    front_shiny: string;
+  };
+};
